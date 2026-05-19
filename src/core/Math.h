a41dbbf21ddbda6f1f9f755a1dfabc2a24f00654
Vallabh Deshpande <vallabh.karad@gmail.com>
2026-05-19 13:44:23 +0530

    Completed Mat4x4 in math.h

--- src/core/Math.h
+++ src/core/Math.h
@@ -5,7 +5,7 @@
 
 namespace Teravox{
 
-    //Defining structure
+    //Defining 3D Vector structure 
 
     struct Vec3{
 
@@ -72,8 +72,125 @@ namespace Teravox{
         //10.Normalized 
         Vec3 normalized()const{
             float mag = magnitude();
+
+            //If magnitude is 0 then return original vector 
+
             if(fabs(mag) < 1e-6f)return Vec3(x,y,z);
+
             return Vec3(x/mag,y/mag,z/mag);
+        }
+    };
+
+    //Defining 2D Vector structure
+
+    struct Vec2{
+
+        float x,y;
+
+        //1.Constructor for 2D Vector
+
+        Vec2(float X = 0.0f, float Y = 0.0f)
+        : x(X), y(Y) {}
+
+        //2.2D Vector addition : overloading '+' operator
+        Vec2 operator+(const Vec2& v)const{
+            return Vec2(x + v.x,y + v.y);
+        }
+
+        //3.2D Vector subtraction : overloading '-' operator
+        Vec2 operator-(const Vec2& v)const{
+            return Vec2(x - v.x,y - v.y);
+        }
+
+        //4.Vector 2D Scalar multiply : overloading '*' operator
+        Vec2 operator*(const float factor)const{
+            return Vec2(x * factor,y * factor);
+        }
+
+        //5.Vector 2D Scalar divide : overloading '/' operator
+        Vec2 operator/(const float factor)const{
+
+            // checking division by zero 
+
+            if(fabs(factor) < 1e-6f)return Vec2(x,y);
+
+            return Vec2(x/factor,y/factor);
+        }
+
+        //6.Vector 2d dot product 
+        float dot(const Vec2& v)const{
+            return x * v.x + y * v.y;
+        }
+
+        //7.Vector 2d magnitude 
+        float magnitude()const{
+            return sqrt((x * x) + (y * y));
+        }
+
+        //8.Vector 2d normalize original vector
+        void normalize(){
+            float mag = magnitude();
+
+            if(fabs(mag) < 1e-6f)return ;
+
+            x /= mag;
+            y /= mag;
+
+            return ;
+        }
+
+        //9.Vector 2d normalized 
+        Vec2 normalized()const{
+
+            float mag = magnitude();
+
+            // If magnitude is 0 then return original vector //
+
+            if(fabs(mag) < 1e-6f)return Vec2(x,y);
+
+            return Vec2(x/mag,y/mag);
+        }
+    };
+
+    //Defining Vector 4 structure
+
+    struct Vec4{
+
+        float x,y,z,w;
+
+        //1.Constructor 
+
+        Vec4(float X = 0.0f,float Y = 0.0f,float Z = 0.0f,float W = 0.0f)
+        : x(X), y(Y), z(Z), w(W) {}
+
+        //2.Vector 4 Addtion : overloading '+' operator
+        Vec4 operator+(const Vec4& v)const{
+            return Vec4(x + v.x,y + v.y,z + v.z,w + v.w);
+        }
+
+        //3.Vector 4 Subtraction : overloading '-' operator
+        Vec4 operator-(const Vec4& v)const{
+            return Vec4(x - v.x,y - v.y,z - v.z,w - v.w);
+        }
+
+        //4.Vector 4 Scalar multiply : overloading '*' operator
+        Vec4 operator*(const float factor)const{
+            return Vec4(x * factor,y * factor,z * factor,w * factor);
+        }
+
+        //5.Vector 4 Scalar divide : overloading '/' operator
+        Vec4 operator/(const float factor)const{
+
+            // checking division by zero 
+
+            if(fabs(factor) < 1e-6f)return Vec4(x,y,z,w);
+
+            return Vec4(x/factor,y/factor,z/factor,w/factor);
+        }
+
+        //6.Vector 4 dot product
+        float dot(const Vec4& v)const{
+            return x * v.x + y * v.y + z * v.z + w * v.w;
         }
     };
 }
